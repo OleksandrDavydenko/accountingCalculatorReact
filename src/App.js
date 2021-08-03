c2154875f415d9c1c830c192dd206273c2bd4cff
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-08-03 13:33:47 +0300

    accaontingCalc first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import classes from'./App.module.scss';
+import SalaryComponent from './components/salary_calculator/SalaryComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classes.App}>
+      <header className={classes['App-header']}>
+        <img 
+          src={logo} 
+          className={classes["App-logo"]} 
+          alt="logo" 
+        />
+        <p>Accounting Calculator</p>
       </header>
+      <SalaryComponent />
     </div>
   );
 }
---
+++ src/components/salary_calculator/SalaryComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import clasess from './SalaryComponent.module.scss';
+
+
+const SalaryComponent = () => {
+
+    return (
+        <div className={clasess.test_class}></div>
+    )
+
+}
+
+export default SalaryComponent
