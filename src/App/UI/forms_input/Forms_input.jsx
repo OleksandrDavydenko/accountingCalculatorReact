82719c2f299edb5422fa23c532a570b43bb565a5
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-08-19 14:57:44 +0300

    setSalary

--- src/App/UI/forms_input/Forms_input.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import classes from './forms_input.module.scss'
-
-export const FromsInput = () => {
-    return <input type="text" className={classes.input_view} />
-}
--- src/App/components/salary_calculator/Salary.jsx
+++ src/App/components/salary_calculator/Salary.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import classes from './salary.module.scss';
-import {FromsInput} from '../../UI/forms_input/Forms_input'
-
-
+import { useState } from 'react';
+import SALARY_CONFIG from './salaryConfig';
 
  export const Salary = () => {
+
+     let [dirty, setDirty] = useState('');
+     let [pure, setPure] = useState('')
+
+    function calculateValue(dirty, pure) {
+
+        let newDirty = (dirty - (dirty * SALARY_CONFIG.ndflPercent) -(dirty * SALARY_CONFIG.vsPercent)).toFixed(2)
+
+        let newPure = (pure /0.805).toFixed(2)
+        return [newDirty, newPure]
+    }
+
+    function handleChangeDirty (event) {
+        setDirty(dirty = event.target.value)
+        setPure(pure = calculateValue(dirty, pure)[0])
+        
+    }
+    function handleChangePure (event) {
+        setPure(pure = event.target.value)
+        setDirty(dirty = calculateValue(dirty, pure)[1])
+    }
 
     return (
         <div >
             <h1>Розрахунок заробітної плати</h1>
              <div className={classes.form_container}>
-                 <FromsInput />
-                 <FromsInput />
+                <input 
+                    type="text" 
+                    className={classes.input_view}
+                    value={dirty}
+                    onChange={handleChangeDirty} 
+                />
+                  <input 
+                    type="text" 
+                    className={classes.input_view}
+                    value={pure}
+                    onChange={handleChangePure} 
+                />
              </div>
         </div>
     )
