74f427c4a83a819f771115ef69b9801f97a6542b
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-10-03 21:26:24 +0300

    dropdown

--- src/App/UI/Dropdown/Dropdown.jsx
+++ src/App/UI/Dropdown/Dropdown.jsx
@@ -1,16 +1,46 @@
-import React from "react";
-import classes from './dropdown.module.scss'
-import {MenuButton} from '../menu_button/MenuButton'
+import React, { useState } from "react";
+import classes from './dropdown.module.scss';
+import cn from 'classnames';
+import { MenuButton } from '../menu_button/MenuButton';
 
 
 export const Dropdown = () => {
+
+    const VATPercents = [20, 7, 14, 0];
+
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    const [activePercent, setActivePercent] = useState(20);
+
+    function toggleDropdownHeandler() {
+        setToggleDropdown(!toggleDropdown)
+    }
+
+    const listItems = VATPercents.map((percent) =>
+        <li
+            className={classes.item}
+            onClick={() => setActivePercent(percent)}
+        >
+            {percent + '%'}</li>
+    );
+
+    let active_cls = classes.dropdown_items;
+
+    if (toggleDropdown) {
+        active_cls = cn(active_cls, classes.active)
+    }
+    
+
     return (
         <div className={classes.dropdown_conteiner}>
-            <div className={classes.input_view} >
-                {'20 %'}
+            <div
+                className={classes.input_view}
+                onClick={toggleDropdownHeandler}
+            >
+                {activePercent + '%'}
                 <div className={classes.dropdown_button}>
-                   <MenuButton/> 
+                    <MenuButton/> 
                 </div>
+                <ul className={active_cls}>{ listItems }</ul>
             </div>
         </div>
     )
