f3ccef2c2036d860334ef81400e541a7830305ec
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-10-17 23:29:10 +0300

    added calculateVAT

--- src/App/components/vat_calculator/ValueAddedTax.jsx
+++ src/App/components/vat_calculator/ValueAddedTax.jsx
@@ -3,19 +3,41 @@ import classes from './valueAddedTax.module.scss';
 import {VATPercents} from './vatConfig'
 
 import { Dropdown } from '../../UI/Dropdown/Dropdown';
+import checkValue from '../../utils/checkValue';
+import calculateVAT from './calculateVAT'
 
 
 export const ValueAddedTax = () => {
 
     const [activePercent, setActivePercent] = useState(20);
-    
+
+    //'бумеранг', получаю значение со стейта c компонента дропдаун.
     const changedData = (percent) => {
         setActivePercent(percent)
     }
-    
+
     console.log(activePercent);
 
+    let [config, setPConfig] = useState({ withoutVAT: "", VAT: "", withVAT: "", activePercent: activePercent });
     
+    function handleWithoutVAT(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateVAT({ withoutVAT: checkedValue, activePercent}))
+        console.log({ withoutVAT: checkedValue });
+    }
+     function handleVAT(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateVAT({ VAT: checkedValue, activePercent }))
+        console.log({ VAT: checkedValue });
+     }
+    function handlewithVAT(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateVAT({ withVAT: checkedValue, activePercent }))
+        console.log({ withVAT: checkedValue });
+    }
+
+
+
 
 return (
     <div >
@@ -36,8 +58,8 @@ return (
                 type="text"
                 placeholder="0.00"
                 className={classes.input_view}
-                value={0}
-                onChange={0} 
+                value={config.newWithoutVAT || ""}
+                onChange={handleWithoutVAT} 
             />
             <div className={classes.title_box}>
                 <h6 className={classes.title}>Сума ПДВ:</h6>
@@ -46,8 +68,8 @@ return (
                 type="text"
                 placeholder="0.00"
                 className={classes.input_view}
-                value={0}
-                onChange={0} 
+                value={config.newVAT || ""}
+                onChange={handleVAT} 
             />
             <div className={classes.title_box}>
                 <h6 className={classes.title}>Загальна сума, з ПДВ:</h6>
@@ -56,8 +78,8 @@ return (
                 type="text"
                 placeholder="0.00"
                 className={classes.input_view}
-                value={0}
-                onChange={0} 
+                value={config.newWithVAT || ""}
+                onChange={handlewithVAT} 
             />
             </div>
     </div>
