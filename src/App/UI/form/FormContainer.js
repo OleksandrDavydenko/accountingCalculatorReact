8112ed51b33c4faa98bf0f79fceae77a0334a8ea
OleksandrDavydenko <oleksandr.davydenko3@gmail.com>
2021-10-22 13:40:14 +0300

    test

--- src/App/UI/form/FormContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './form.module.scss'
-
-export const FormContainer = () => {
-
-    return (
-        <div className={classes.form_container}></div>
-    )
-}
--- src/App/components/salary_calculator/Salary.jsx
+++ src/App/components/salary_calculator/Salary.jsx
@@ -1,49 +1,82 @@
 import React from 'react';
 import classes from './salary.module.scss';
 import { useState } from 'react';
-import SALARY_CONFIG from './salaryConfig';
+import checkValue from '../../utils/checkValue';
+import calculateValue from './calculateValue';
 
  export const Salary = () => {
-
-     let [dirty, setDirty] = useState('');
-     let [pure, setPure] = useState('')
-
-    function calculateValue(dirty, pure) {
-
-        let newDirty = (dirty - (dirty * SALARY_CONFIG.ndflPercent) -(dirty * SALARY_CONFIG.vsPercent)).toFixed(2)
-
-        let newPure = (pure /0.805).toFixed(2)
-        return [newDirty, newPure]
+  
+     let [config, setPConfig] = useState({newDirtySalary: '', newPureSalary: '', newPdfo: '', newMilitaryTax: ''});
+  
+     function handleChangeDirty(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateValue({ dirtySalary: checkedValue }))
+        console.log(calculateValue({ dirtySalary: checkedValue }));
     }
 
-    function handleChangeDirty (event) {
-        setDirty(dirty = event.target.value)
-        setPure(pure = calculateValue(dirty, pure)[0])
-        
-    }
-    function handleChangePure (event) {
-        setPure(pure = event.target.value)
-        setDirty(dirty = calculateValue(dirty, pure)[1])
-    }
+     function handleChangePure(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateValue({pureSalary: checkedValue}))
+     }
+     
+     function handleChangePdfo(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateValue({ pdfo: checkedValue }))
+        console.log(calculateValue({ pdfo: checkedValue }));
+     }
+
+     function handleMilitaryTax(event) {
+        const checkedValue = checkValue(event.target.value)
+        setPConfig(calculateValue({ militaryTax: checkedValue }))
+        console.log(calculateValue({ militaryTax: checkedValue }));
+     }
+
 
     return (
         <div >
             <h1>Розрахунок заробітної плати</h1>
-             <div className={classes.form_container}>
-                <input 
+            <div className={classes.form_container}>
+                <div className={classes.title_box}>
+                    <h6 className={classes.title}>Оклад:</h6>
+                </div>
+                <input
+                    placeholder="0.00"
                     type="text" 
                     className={classes.input_view}
-                    value={dirty}
+                    value={config.newDirtySalary}
                     onChange={handleChangeDirty} 
                 />
-                  <input 
-                    type="text" 
+                <div className={classes.title_box}>
+                    <h6 className={classes.title}>На руки:</h6>
+                </div>          
+                <input 
+                    type="text"
+                    placeholder="0.00"
                     className={classes.input_view}
-                    value={pure}
+                    value={config.newPureSalary}
                     onChange={handleChangePure} 
+                />
+                <div className={classes.title_box}>
+                    <h6 className={classes.title}>Податок з доходу фізичних осіб:</h6>
+                </div>          
+                <input 
+                    type="text"
+                    placeholder="0.00"
+                    className={classes.input_view}
+                    value={config.newPdfo}
+                    onChange={handleChangePdfo} 
+                />
+                <div className={classes.title_box}>
+                    <h6 className={classes.title}>Військовий збір:</h6>
+                </div>          
+                <input 
+                    type="text"
+                    placeholder="0.00"
+                    className={classes.input_view}
+                    value={config.newMilitaryTax}
+                    onChange={handleMilitaryTax} 
                 />
              </div>
         </div>
     )
-
 }
